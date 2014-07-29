d756cbd69d22c4c6bca2c1fc94603df8fc7cd82b
RJWorth <bot.collins@gmail.com>
2014-07-29 15:31:44 -0400

    Adding proxlike directory to back it up

---
+++ Sims/Proxlike/071714/PrxA/extend.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+############################################################################### 
+### after modifying param.dmp, run this to restart the simulation
+### with folder name as first argument, merc name as second. Example:
+### ./extend.bash 5-2 CDir4 > run.pipe &
+
+t1=$(date +%s)
+
+cd $1/Out
+	./merc_AC$2
+	./elem
+	\mv *.aei AeiOutFiles
+cd ../..
+
+t2=$(date +%s)
+echo $1"	"$(echo "$t2 - $t1"|bc ) >> runtime.txt
+
+cd ..; ./email.sh $1 '' 'AC extension finished'; cd Prx1
+
