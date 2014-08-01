eff89ca3e60295bccb9b84579495b1373b7f8d8d
RJWorth <bot.collins@gmail.com>
2014-08-01 13:32:03 -0400

    added line to run FinishBroken.sh from master.bash

--- Sims/master.bash
+++ Sims/master.bash
@@ -11,11 +11,11 @@
 machine=$(hostname -s)
 if [ $machine = chloe ]; then
 #	Dirs=(CDir1 CDir2 CDir3 CDir4 CDir5 CDir6 CDir7 CDir8 CDir9 CDir10)
-	Dirs=()
+	Dirs=(CDir1 CDir2 CDir3 CDir4 CDir5 CDir6 CDir7 CDir9 CDir10)
 fi
 if [ $machine = shapiro ]; then
 #	Dirs=(SDir1 SDir2 SDir3 SDir4 SDir5 SDir6 SDir7)
-	Dirs=()
+	Dirs=(SDir3 SDir4 SDir5 SDir6 SDir7)
 fi
 #if [ $machine = spaatz ]; then
 #	Dirs=(PDir1 PDir2 PDir3)
@@ -27,7 +27,8 @@ fi
 for i in ${Dirs[*]}
 do
 	
-	nice -n 10 ./run.sh $i > $i/run.pipe &
+#	nice -n 10 ./run.sh $i > $i/run.pipe &
+	nice -n 10 ./FinishBroken.sh $i >> $i/run.pipe &
 	echo 'master: '$i'  '$!
 done
 
