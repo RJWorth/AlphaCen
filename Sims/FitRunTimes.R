09d4f913c53adf43123825d1e735df823d7b31d2
RJWorth <bot.collins@gmail.com>
2014-08-13 12:35:15 -0400

    improved plot, extended model to 1e8

--- Sims/FitRunTimes.R
+++ Sims/FitRunTimes.R
@@ -2,7 +2,7 @@ times=read.table('looptimes.txt',skip=1)
 colnames(times)=c('dir','machine','it','logt','date','time','dt')
 attach(times)
 
-logsimt=3:7
+logsimt=3:8
 simt=10^logsimt
 realt=simt
 for (i in 1:length(simt)) realt[i]=mean(dt[logt==(i+2)])
@@ -20,9 +20,12 @@ timevalues <- seq(min(logsimt), max(logsimt), length.out=10)
 realt.exp1 <-     10^(predict(      exp.model,list(logsimt=timevalues)))
 realt.exp2 <- 10^(10^(predict(doubleexp.model,list(logsimt=timevalues))))
 
-plot(logsimt, realt, pch=20)
-lines(timevalues, realt.exp1, col = "blue")
-lines(timevalues, realt.exp2, col = "red")
+plot(10^logt,dt, pch=20, log='xy', xlab='sim time (yrs)',ylab='walltime (s)')
+points(10^logsimt, realt, pch=20,col='red')
+lines(10^timevalues, realt.exp1, col = "blue")
+lines(10^timevalues, realt.exp2, col = "green")
 
+legend('topleft',legend=c('actual times','average times','walltime ~ (sim t)^n','walltime ~ m^((sim t)^n)'),
+	col=c('black','red','blue','green'),lty=c(0,0,1,1),pch=c(20,20,NA,NA))
 
 detach(times)
