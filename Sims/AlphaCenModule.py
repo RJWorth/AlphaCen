bba4d8bc19c31b63c9ed6af5974a3380a5edc4f3
RJWorth <bot.collins@gmail.com>
2014-08-21 14:43:59 -0400

    Imported el2x from mercury, and elliptical kepler eqn solver

--- Sims/AlphaCenModule.py
+++ Sims/AlphaCenModule.py
@@ -113,37 +113,262 @@ def MakeBigRand(WhichDir,WhichTime, cent,
 	InParams.close()
 
 ###########################################################################
+### Read in big.in parameters
+
+###########################################################################
+### Convert from cartesian (xyz uvw) to orbital elements (aei gnM)
+#   BUT WHAT FRAME ARE THEY IN???
+def El2X(a,e,i,g,n,l):
+'''Convert orbital elements to cartesian for an ellipse (e < 1). 
+Based on MCO_EL2X.FOR from J. Chambers' mercury6_2.for:
+	gm = grav const * (central + secondary mass)
+	q = perihelion distance
+	e = eccentricity
+	i = inclination                 )
+	p = longitude of perihelion !!! )   in
+	n = longitude of ascending node ) radians
+	l = mean anomaly                )
+
+	x,y,z = Cartesian positions  ( units the same as a )
+	u,v,w =     "     velocities ( units the same as sqrt(gm/a) )'''
+# Big.in format:
+# Asteroidal = Keplerian orbital elements, in an `asteroidal' format.
+#              i.e.  a e I g n M, where
+#               a = semi-major axis (in AU)
+#               e = eccentricity
+#               I = inclination (degrees)
+#               g = argument of pericentre (degrees)
+#               n = longitude of the ascending node (degrees)
+#               M = mean anomaly (degrees)
+
+### Modules used
+	import numpy as np
+	from numpy import sqrt, sin, cos
+	from mks_constants import deg2rad,rad2deg
+
+### Convert input degrees to radians
+	i, g, n, m = deg2rad*i, deg2rad*g, deg2rad*n, deg2rad*m
+
+### Pericenter
+	q = (1.-e)*a
+
+### Get p (longitude of pericenter) from g (argument of pericenter)
+	p = g + n
+
+### Rotation factors
+	si, ci = sin(i), cos(i)
+	sg, cg = sin(g), cos(g)
+	sn, cn = sin(n), cos(n)
+	z1 = cg * cn
+	z2 = cg * sn
+	z3 = sg * cn
+	z4 = sg * sn
+	d11 =  z1 - z4*ci
+	d12 =  z2 + z3*ci
+	d13 =       sg*si
+	d21 = -z3 - z2*ci
+	d22 = -z4 + z1*ci
+	d23 =       cg*si
+
+### Ellipse
+	if (e == 1.0):
+		romes = sqrt(1.0 - e*e)
+		temp = mco_kep (e,l)
+		se, ce = sin(temp), cos(temp)
+		z1 = a * (ce - e)
+		z2 = a * romes * se
+		temp = sqrt(gm/a) / (1.0 - e*ce)
+		z3 = -se * temp
+		z4 = romes * ce * temp
+	elif (e == 1.0) then
+### Parabola
+		ce = orbel_zget(l)
+		z1 = q * (1.0 - ce*ce)
+		z2 = 2.0 * q * ce
+		z4 = sqrt(2.0*gm/q) / (1.0 + ce*ce)
+	 	z3 = -ce * z4
+	else:
+### Hyperbola
+		romes = sqrt(e*e - 1.0)
+		temp = orbel_fhybrid(e,l)
+		se, ce = sin(temp), cos(temp)
+		z1 = a * (ce - e)
+		z2 = -a * romes * se
+		temp = sqrt(gm/abs(a)) / (e*ce - 1.0)
+		z3 = -se * temp
+		z4 = romes * ce * temp
+###
+	x = d11 * z1  +  d21 * z2
+	y = d12 * z1  +  d22 * z2
+	z = d13 * z1  +  d23 * z2
+	u = d11 * z3  +  d21 * z4
+	v = d12 * z3  +  d22 * z4
+	w = d13 * z3  +  d23 * z4
+
+	return(x,y,z,u,v,w)
+
+###############################################################################
+def KeplerEllipse():
+'''Solves Kepler's equation for eccentricities less than one.
+ Algorithm from A. Nijenhuis (1991) Cel. Mech. Dyn. Astron. 51, 319-330.
+
+  e = eccentricity
+  l = mean anomaly      (radians)
+  u = eccentric anomaly (   "   )'''
+#------------------------------------------------------------------------------
+
+	from math import pi, exp, log, sin, cos
+	twopi = 2.*pi
+	piby2 = pi/2.
+
+### Reduce mean anomaly to lie in the range 0 < l < pi
+	if (oldl >= 0):
+		l = oldl % twopi
+	else:
+		l = oldl % twopi + twopi
+	sign = 1.0
+	if (l > pi):
+		l = twopi - l
+		sign = -1.d0
+
+	ome = 1.d0 - e
+#-----------------------------
+	if (l >= .45 | e < .55):
+### Regions A,B or C in Nijenhuis
+
+### Rough starting value for eccentric anomaly
+		if (l < ome):
+			u1 = ome
+		elif (l > (pi-1.0-e)):
+			u1 = (l+e*pi)/(1.0+e)
+	    else:
+			u1 = l + e
+
+### Improved value using Halley's method
+		flag = u1 > piby2
+		if (flag):
+			x = pi - u1
+		else:
+			x = u1
+		x2 = x*x
+		sn = x*(1.0 + x2*(-.16605 + x2*.00761) )
+		dsn = 1.0 + x2*(-.49815 + x2*.03805)
+		if (flag):
+			dsn = -dsn
+		f2 = e*sn
+		f0 = u1 - f2 - l
+		f1 = 1.d0 - e*dsn
+		u2 = u1 - f0/(f1 - .5d0*f0*f2/f1)
+
+#---------------------
+	else:
+### Region D in Nijenhuis
+### Rough starting value for eccentric anomaly
+		z1 = 4.0*e + .50
+		p = ome / z1
+		q = .50 * l / z1
+		p2 = p*p
+		z2 = exp( log( sqrt( p2*p + q*q ) + q )/1.5 )
+		u1 = 2.0*q / ( z2 + p + p2/z2 )
+
+### Improved value using Newton's method
+		z2 = u1*u1
+		z3 = z2*z2
+		u2 = u1 - .075*u1*z3 / (ome + z1*z2 + .375*z3)
+		u2 = l + e*u2*( 3.0 - 4.0*u2*u2 )
+
+### Accurate value using 3rd-order version of Newton's method
+### N.B. Keep cos(u2) rather than sqrt( 1-sin^2(u2) ) to maintain accuracy!
+
+### First get accurate values for u2 - sin(u2) and 1 - cos(u2)
+	bigg = (u2 > piby2)
+	if (bigg):
+		z3 = pi - u2
+	else:
+		z3 = u2
+
+	big = (z3 > (.5*piby2))
+	if (big):
+		x = piby2 - z3
+	else:
+		x = z3
+
+	x2 = x*x
+	ss = 1.0
+	cc = 1.0
+
+	ss = x*x2/6.*(1. - x2/20.*(1. - x2/42.*(1. - x2/72.*(1. -
+     %   x2/110.*(1. - x2/156.*(1. - x2/210.*(1. - x2/272.)))))))
+	cc =   x2/2.*(1. - x2/12.*(1. - x2/30.*(1. - x2/56.*(1. -
+     %   x2/ 90.*(1. - x2/132.*(1. - x2/182.*(1. - x2/240.*(1. -
+     %   x2/306.))))))))
+
+	if (big):
+		z1 = cc + z3 - 1.0
+		z2 = ss + z3 + 1.0 - piby2
+	else:
+		z1 = ss
+		z2 = cc
+
+	if (bigg):
+		z1 = 2.0*u2 + z1 - pi
+		z2 = 2.0 - z2
+
+	f0 = l - u2*ome - e*z1
+	f1 = ome + e*z2
+	f2 = .5*e*(u2-z1)
+	f3 = e/6.0*(1.0-z2)
+	z1 = f0/f1
+	z2 = f0/(f2*z1+f1)
+	mco_kep = sign*( u2 + f0/((f3*z1+f2)*z2+f1) )
+
+	return(mco_kep)
+
+###############################################################################
+### Add variations in pos/vel to a central/starting point
+
+
+
+###########################################################################
 ### Make a random cluster of small objects around preselected ones
 ### to write to small.in
-def MakeSmallTestDisk(WhichDir,WhichTime,n,planet,da,dv):
+def MakeSmallTestDisk(WhichDir,WhichTime,n,m,planet,da,dv):
 	n=int(n)
 ### Needed modules
 	import numpy as np
 	import os, filelen
 	from random import random
 	from math   import pi, sin, cos
-	from mks_constants import AU, day
+	from mks_constants import AU, day, mSun
 
 	here=os.getcwd()
 	
-	print('MakeSmallTestDisk '+WhichDir+'/In/small.in  '+WhichTime+'  n='+str(n))
+	print('MakeSmallTestDisk {0}/In/small.in  {1}  n={2}'.format(
+			WhichDir, WhichTime, n))
 
 ### Constants/variables
 #	AU = 1.496e13					#cm/AU
 #	day = 24.*3600.					#s/day
+
+### Parameters
+	m = m/mSun						# mSun
+	r = 0.001						# Hill radii for interaction
+	d = 2.0							# g/cm^3
 	maxaspread=da/AU				#in AU
 	maxvspread=dv*day/AU			#in AU/day
 	
-	small=['M'+str(i) for i in range(n)]
+	names=['M'+str(i) for i in range(n)]
 	smallxv=['' for i in range(n)]
 
 ### Generate slightly randomized rocks at different phases of Jupiter or Saturn's orbit
-	for j in range(0,len(small)):
+	for j in range(0,len(names)):
 		### Pick a random timestep
 		if planet=='J':
-			filename=here+'/'+WhichDir+'/In/InitElemFiles/Jupiter12Yr.aei'
+			filename='{0}/{1}/In/InitElemFiles/Jupiter12Yr.aei'.format(
+						here,WhichDir)
 		if planet=='S':
-			filename=here+'/'+WhichDir+'/In/InitElemFiles/Saturn29Yr.aei'
+			filename='{0}/{1}/In/InitElemFiles/Saturn29Yr.aei'.format(
+						here,WhichDir)
 		AEILen=filelen.file_len(filename)-5
 		timestep=5+int(AEILen*random())
 		### Get Jupiter/Saturn's info at this point
@@ -168,25 +393,28 @@ def MakeSmallTestDisk(WhichDir,WhichTime,n,planet,da,dv):
 		w=v*sin(theta2)
 
 		### Coords = Jupiter/Saturn coords plus random variation
-		smallxv[j]=[float(bigxv[0])+x, float(bigxv[1])+y, float(bigxv[2])+z,
+		SmlXV[j]=[float(bigxv[0])+x, float(bigxv[1])+y, float(bigxv[2])+z,
 		float(bigxv[3])+u, float(bigxv[4])+v, float(bigxv[5])+w]
 
-### Write small.in
-	smallheader="    m=1.0012066e-17  r=0.001 d=2.0\n"
-	smalls="  0.0 0.0 0.0\n"
-	smallfile=open(whichdir+'/In/small.in','w')
-	smallfile.write(")O+_06 Small-body initial data  (WARNING: Do not delete this line!!)\n")
-	smallfile.write(") Lines beginning with `)' are ignored.\n")
-	smallfile.write(")---------------------------------------------------------------------\n")
-	smallfile.write("style (Cartesian, Asteroidal, Cometary) = Cartesian\n")
-	smallfile.write(")---------------------------------------------------------------------\n")
-	for j in range(len(small)):
-		smallfile.write(small[j]+smallheader)
-		smallfile.write("  "+repr(smallxv[j][0])+"  "+repr(smallxv[j][1])+"  "+repr(smallxv[j][2])+"\n")
-		smallfile.write("  "+repr(smallxv[j][3])+"  "+repr(smallxv[j][4])+"  "+repr(smallxv[j][5])+"\n")
-		smallfile.write(smalls)
+### Read generic small.in file header	
+	SmlHeadFile=open('SmlHeader.txt','r')
+	SmlHeader=SmlHeadFile.readlines()
+	SmlHeadFile.close()
 
-	smallfile.close()
+### First lines for each object
+	SmlParams="    m={0}  r={1} d={2}\n".format(m, r, d)
+	SmlFirstLines=[]
+	for i in names:
+		SmlFirstLines.append(i+SmlParams)
+	print(SmlFirstLines)
+
+### Spin
+### No spin for all objects
+	SmlS=["  0.0  0.0  0.0\n" for i in range(len(SmlFirstLines))]
+
+### Write big file
+	AC.WriteObjInFile(
+	WhichDir,names,'small',SmlHeader,SmlFirstLines,SmlXV,SmlS)
 
 ###############################################################################
 def InitParams(WhichDir):
