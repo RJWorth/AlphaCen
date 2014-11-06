d7a23406c47a0b8bd265ee1727a7dd4168ac9ddc
RJWorth <bot.collins@gmail.com>
2014-11-06 16:46:48 -0500

    fix to Scp script

--- Sims/ScpPrxDirs.bash
+++ Sims/ScpPrxDirs.bash
@@ -3,14 +3,14 @@
 # scp all the specified directories into the analogous location here
 
 pre='Proxlike/Prx'
-Dir=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
+Dir=(01) # 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
 suf=('/DiskA-2' '/DiskA-3' '/DiskB-2' '/DiskB-3')
 
 for i in ${Dir[*]}; do
 	for j in ${suf[*]}; do
 		d=$pre$i$j
-		echo rjw274@lionxj.astro.psu.edu:'~/work/AlphaCen/Sims/'$d/*.pdf $d
-#		scp -rp rjw274@lionxj.astro.psu.edu:'~/work/AlphaCen/Sims/'$d/*.pdf $d 		
+		echo rjw274@lionxj.rcc.psu.edu:'~/work/AlphaCen/Sims/'$d/*.pdf $d
+		scp -rp rjw274@lionxj.rcc.psu.edu:'~/work/AlphaCen/Sims/'$d/*.pdf $d 		
 #		scp -rp rjw274@shapiro.astro.psu.edu:'~/AlphaCen/Sims/'$d $pre$i
 	done
 done
