ee5a44102bab8fe6713aff3d96a08c440cef2a8c
RJWorth <bot.collins@gmail.com>
2014-11-06 10:53:23 -0500

    R script for reading/plotting/fitting disk runtimes

---
+++ Sims/ReadDisktimes.R
@@ -0,0 +1,24 @@
+disktimes=read.table('disktimes.txt')
+	colnames(disktimes)=c('dir','logt','wallt')
+attach(disktimes)
+
+logst=3:7
+st=10^logst
+wt=rep(NA,length(st))
+
+for (i in 1:length(st)) wt[i] = max(wallt[ logt==logst[i]] ])
+logwt=log10(wt)
+
+fit = lm(logwt ~ logst)
+b = fit$coef[1]
+m = fit$coef[2]
+
+### predictions
+logpst = 3:9
+pst = 10^logpst
+pwt = 10^b * pst^m
+
+print(paste('1e6 sims done in',pwt[4]/3600/24,'days'))
+print(paste('1e7 sims done in',pwt[5]/3600/24,'days'))
+print(paste('1e8 sims done in',pwt[6]/3600/24,'days'))
+print(paste('1e9 sims done in',pwt[7]/3600/24,'days'))
