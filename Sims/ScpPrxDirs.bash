61d0dbe7374f2e5fb32bb230c630e5d36e57a381
RJWorth <bot.collins@gmail.com>
2014-11-12 11:02:32 -0500

    updated bash scripts for most recent need

--- Sims/ScpPrxDirs.bash
+++ Sims/ScpPrxDirs.bash
@@ -3,11 +3,11 @@
 # scp all the specified directories into the analogous location here
 
 pre='Proxlike/Prx'
-#Dir=(03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
-suf=('/DiskA-2' '/DiskA-3' '/DiskB-2' '/DiskB-3')
+Dir=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
+#suf=('/DiskA-2' '/DiskA-3' '/DiskB-2' '/DiskB-3')
 
 for i in ${Dir[*]}; do
-	for j in ${suf[*]}; do
+#	for j in ${suf[*]}; do
 		d=$pre$i$j
 #		mkdir -p $d
 #		echo    rjw274@lionxj.rcc.psu.edu:'~/work/AlphaCen/Sims/'$d/*.pdf $d
@@ -17,5 +17,5 @@ for i in ${Dir[*]}; do
 		echo R CMD BATCH -$d '../Analysis/ReadDisk.R'
 		R CMD BATCH -$d '../Analysis/ReadDisk.R'
 		mv ReadDisk.Rout $d
-	done
+#	done
 done
