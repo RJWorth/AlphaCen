62d3a6783395a92b8f489cfe3a21920953d92b10
RJWorth <rjw274@psu.edu>
2014-11-17 10:36:53 -0500

    ran 1e7 extensions of sims

--- Sims/DiskMaster.bash
+++ Sims/DiskMaster.bash
@@ -10,29 +10,28 @@ newrun=F
 sim='Proxlike/Prx'
 dir='/Disk'
 ### Assign directories based on machine
-if [ ${machine:0:5} = 'lionx' ]; then
-	echo 'running on '$machine
-	if [ ${machine:5:1} = 'j' ]; then
-	Which=(01 02 03 04 05 06 07)
-	elif  [ ${machine:5:1} = 'g' ]; then
-	Which=(08 09 10 11 12 13 14)
-	elif  [ ${machine:5:1} = 'f' ]; then
-	Which=(15 16 17 18 19 20 21)
-	elif  [ ${machine:5:1} = 'i' ]; then
-	Which=(22 23 24 25 26 27 28)
-	elif  [ ${machine:5:1} = 'h' ]; then
-	Which=(29 30 31 32 33 34)
-	else
-		echo 'unknown lionx machine'
-	fi
-else
-	echo 'Non-lionx machine'
-	Which=()
-fi
+#if [ ${machine:0:5} = 'lionx' ]; then
+#	echo 'running on '$machine
+#	if [ ${machine:5:1} = 'j' ]; then
+#	Which=(01 02 03 04 05 06 07)
+#	elif  [ ${machine:5:1} = 'g' ]; then
+#	Which=(08 09 10 11 12 13 14)
+#	elif  [ ${machine:5:1} = 'f' ]; then
+#	Which=(15 16 17 18 19 20 21)
+#	elif  [ ${machine:5:1} = 'i' ]; then
+#	Which=(22 23 24 25 26 27 28)
+#	elif  [ ${machine:5:1} = 'h' ]; then
+#	Which=(29 30 31 32 33 34)
+#	else
+#		echo 'unknown lionx machine'
+#	fi
+#else
+#	echo 'Non-lionx machine'
+#	Which=()
+#fi
+#Which=(01 03 05 06 07 09 10 11 12 13 14 15 16 17 19 20 21 22 23 24 26 33)
+Which=()
 echo ${Which[*]}
-#sim=''
-#dir=''
-Which=(01)
 
 ### Simulation parameters
 mA=0.123	#1.105
@@ -49,38 +48,38 @@ do
 	j=$sim$i$dir
 
 ### Set up triple system sim (disk around A)
-	cent='AlCenA'
-	if [ $newrun = T ]; then
-		if [ ! -d $j'A-3' ]; then
-			echo 'Creating '$j'A-3'
-			\cp -rp $sim$i/Original $j'A-3'
-		else
-			echo $j'A-3' exists
-		fi
-                \rm $j'A-3'/run.pipe
-		# If no backup dir yet, make one
-		mkdir -p $j'A-3'/Out/Backup
-		# Reset stop files
-		echo 'False' > $j'A-3'/stopfile.txt
-		echo 'False' > $j'A-3'/bigstopfile.txt
-
-		# Generate disk
-		if [ $newdisk = T ]; then
-		python -c 'import AlphaCenModule as AC; AC.MakeSmallTestDisk("'$j'A-3",centobj="'$cent'")'
-		fi
-	fi	
+#	cent='AlCenA'
+#	if [ $newrun = T ]; then
+#		if [ ! -d $j'A-3' ]; then
+#			echo 'Creating '$j'A-3'
+#			\cp -rp $sim$i/Original $j'A-3'
+#		else
+#			echo $j'A-3' exists
+#		fi
+ #               \rm $j'A-3'/run.pipe
+#		# If no backup dir yet, make one
+#		mkdir -p $j'A-3'/Out/Backup
+#		# Reset stop files
+#		echo 'False' > $j'A-3'/stopfile.txt
+#		echo 'False' > $j'A-3'/bigstopfile.txt
+#
+#		# Generate disk
+#		if [ $newdisk = T ]; then
+#		python -c 'import AlphaCenModule as AC; AC.MakeSmallTestDisk("'$j'A-3",centobj="'$cent'")'
+#		fi
+#	fi	
 
 ### Set up binary system sim (disk around A)
-	if [ $newrun = T ]; then
-		if [ -d $j'A-2' ]; then
-			\rm -r $j'A-2'
-		fi
-		echo 'Creating '$j'A-2'
-		\cp -rp $j'A-3' $j'A-2'
-
-		head -10 $j'A-3'/In/big.in > $j'A-2'/In/big.in
-	fi
-
+#	if [ $newrun = T ]; then
+#		if [ -d $j'A-2' ]; then
+#			\rm -r $j'A-2'
+#		fi
+#		echo 'Creating '$j'A-2'
+#		\cp -rp $j'A-3' $j'A-2'
+#
+#		head -10 $j'A-3'/In/big.in > $j'A-2'/In/big.in
+#	fi
+#
 ### Set up triple system sim (disk around B)
 	cent='AlCenB'
 	if [ $newrun = T ]; then
@@ -101,7 +100,12 @@ do
 		if [ $newdisk = T ]; then
 		python -c 'import AlphaCenModule as AC; AC.MakeSmallTestDisk("'$j'B-3",centobj="'$cent'")'
 		fi
-	fi	
+	else
+		\cp -p $j'B-3'/Out/*.out $j'B-3'/Out/Backup
+		\cp -p $j'B-3'/Out/*.dmp $j'B-3'/Out/Backup
+		\cp -p $j'B-3'/Out/*.tmp $j'B-3'/Out/Backup
+		echo 'Attempted backup of previous timestep'
+	fi
 
 ### Set up binary system sim (disk around B)
 	if [ $newrun = T ]; then
@@ -112,6 +116,11 @@ do
 		\cp -rp $j'B-3' $j'B-2'
 
 		head -10 $j'B-3'/In/big.in > $j'B-2'/In/big.in
+	else
+		\cp -p $j'B-2'/Out/*.out $j'B-2'/Out/Backup
+		\cp -p $j'B-2'/Out/*.dmp $j'B-2'/Out/Backup
+		\cp -p $j'B-2'/Out/*.tmp $j'B-2'/Out/Backup
+		echo 'Attempted backup of previous timestep'
 	fi
 
 	# Start running triple and binary sims
@@ -127,12 +136,12 @@ do
 		echo 'master: '$j'B-3  '$!
 	elif [ ${machine:0:5} = 'lionx' ]; then
 		echo 'using qsub script'
-		d=${j}A-2
-	        runname=${d:12:2}_${d:19:3}
-		qsub -v h=$home,dir=$d,mA=$mA,newrun=$newrun,runname=$runname run1.pbs
-                d=${j}A-3
-                runname=${d:12:2}_${d:19:3}
-		qsub -v h=$home,dir=$d,mA=$mA,newrun=$newrun,runname=$runname run1.pbs
+#		d=${j}A-2
+#	        runname=${d:12:2}_${d:19:3}
+#		qsub -v h=$home,dir=$d,mA=$mA,newrun=$newrun,runname=$runname run1.pbs
+#               d=${j}A-3
+#               runname=${d:12:2}_${d:19:3}
+#		qsub -v h=$home,dir=$d,mA=$mA,newrun=$newrun,runname=$runname run1.pbs
                 d=${j}B-2
                 runname=${d:12:2}_${d:19:3}
 		qsub -v h=$home,dir=$d,mA=$mA,newrun=$newrun,runname=$runname run1.pbs
