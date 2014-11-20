33c5f9d0007f5bb9836f35e894b623ae54cfd363
RJWorth <bot.collins@gmail.com>
2014-11-20 15:48:16 -0500

    Adapted script to pull images/txt created by R script to local machine

--- Sims/PipeFromCluster.bash
+++ Sims/PipeFromCluster.bash
@@ -4,16 +4,19 @@
 
 pre='Proxlike/Prx'
 #Dir=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34)
-Dir=(29 30 31 32 33 34)
-suf=('/DiskA-3' '/DiskB-3')
+Dir=(11 12 13 14)
+suf=('/DiskB-2' '/DiskB-3')
 
 for i in ${Dir[*]}; do
+	echo $pre$i
+	scp -p rjw274@hammer.rcc.psu.edu:'~/work/AlphaCen/Sims/'$pre$i'/DiskSurv.pdf' $pre$i
+	scp -p rjw274@hammer.rcc.psu.edu:'~/work/AlphaCen/Sims/'$pre$i'/SurvGrid.txt' $pre$i
+
 	for j in ${suf[*]}; do
 		d=$pre$i$j
-		mkdir -p $d
-		echo    '~/work/AlphaCen/Sims/'$d
-		scp -rp rjw274@lionxj.rcc.psu.edu:'~/work/AlphaCen/Sims/'$d'/run.pipe' $d
-#		scp -rp rjw274@shapiro.astro.psu.edu:'~/AlphaCen/Sims/'$d $pre$i
+#		mkdir -p $d
+		echo $d
+		scp -p rjw274@hammer.rcc.psu.edu:'~/work/AlphaCen/Sims/'$d'/DiskSurv.pdf' $d
 
 #		echo R CMD BATCH -$d '../Analysis/ReadDisk.R'
 #		R CMD BATCH -$d '../Analysis/ReadDisk.R'
