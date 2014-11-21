724e68bdf5a70e6ba8a5297fff68cfdbcab9578e
RJWorth <bot.collins@gmail.com>
2014-11-21 12:04:39 -0500

    added variable to tell script which cases to look for

--- Analysis/CompareProxSurvivals.R
+++ Analysis/CompareProxSurvivals.R
@@ -1,19 +1,30 @@
 
 
 n=34
+cases = c('B-2','B-3')
+selection = 'B'
+
+### Make array of directory numbers
 dirs=array(data=NA,dim = c(n))
 for (i in 1:n) dirs[i] = sprintf("%02d", i)
-data = array(data = NA, dim = c(n,4,2), 
-	dimnames = list(dirs,c('A2','A3','B2','B3'),c('surv','stab')) )
 
+### Get number of cases from dimensions of SurvGrid.txt file
+### Note: file also contains 3 footer lines and one column names line
 
+### 3D array for data to be filled into
+data = array(data = NA, dim = c(n,length(cases),2), 
+	dimnames = list(dirs,cases,c('surv','stab')) )
+
+### Read data
 for (i in 1:n)	{
 	print(dirs[i])
-	file = paste('Proxlike/Prx',dirs[i],'/SurvGrid.txt',sep='')
-	readin = read.table(file,header=TRUE,row.names=1,nrows=2)
-	for (j in 1:4)	{
+	GridFile = paste('Proxlike/Prx',dirs[i],'/SurvGrid.txt',sep='')
+	GridLen = length(readLines(GridFile))-4
+	readin = read.table(GridFile,header=TRUE,row.names=1,nrows=GridLen)
+	wantrows = grep(selection,rownames(readin))
+	for (j in 1:length(wantrows))	{
 		for (k in 1:2)	{
-			data[i,j,k] = readin[j,k]
+			data[i,j,k] = readin[wantrows[j],k]
 		}	# k, surv/stab
 	}	# j, disk num.
 	}	# i, dirs
@@ -22,14 +33,13 @@ pdf('Proxlike/PrxDisksSurvival.pdf')
 plot(1:n, data[,1,1], pch=1,col='blue',
 	ylim=c(0,1), main='Final Disk Survival Fractions',
 	xlab='Prx Simulation #', ylab='Fraction surviving/stable')
-	points(1:n, data[,3,1], pch=1, col='blue')
-	points(1:n, data[,2,1], pch=1, col='red')
-	points(1:n, data[,4,1], pch=1, col='red')
-
-	points(1:n, data[,1,2], pch=20, col='blue')
-	points(1:n, data[,3,2], pch=20, col='blue')
-	points(1:n, data[,2,2], pch=20, col='red')
-	points(1:n, data[,4,2], pch=20, col='red')
+	# plot stability in closed circles (20), survival in open (1)
+	# plot binaries in blue, triples in red
+	for (i in 1:length(cases))	{
+		if (length(grep('2',colnames(data)[i]))>0) c='blue' else c='red'
+		points(1:n, data[,i,1], pch= 1, col=c)
+		points(1:n, data[,i,2], pch=20, col=c)
+	}
 
 legend('topright',legend=c('Binary','Triple','Survival','Stability'),
 	pch=c(20,20,1,20),
@@ -40,9 +50,9 @@ dev.off()
 ### Make array of just the average binary and triple stability rates for each
 avgs = array(data=NA, dim = c(n, 4), dimnames = list(dirs,c('Bin','Tri','delta','match')))
 for (i in 1:length(dirs))	{
-	avgs[i,1] = mean(data[i,c(1,3),2])
-	avgs[i,2] = mean(data[i,c(2,4),2])
-	avgs[i,3] = mean(data[i,c(1,3),2]) - mean(data[i,c(2,4),2])
+	avgs[i,1] = mean(data[i, grep('2',colnames(data)) ,2])
+	avgs[i,2] = mean(data[i, grep('3',colnames(data)) ,2])
+	avgs[i,3] = mean(avgs[i,1] - avgs[i,2])
 	}	# i, dirs
 
 ### Read in matching parameter, calculated from Compare.py
