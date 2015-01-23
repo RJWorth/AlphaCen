5102df66b843529beffe1c9df2aaa673b8822cb2
RJWorth <bot.collins@gmail.com>
2015-01-23 13:35:05 -0500

    Script for writing a.out,etc for disk+stars

---
+++ Sims/DiskAE.py
@@ -0,0 +1,70 @@
+WhichDir = 'Proxlike/Prx01/DiskB-2'
+n = 10
+import AlphaCenModule as AC
+import numpy as np
+from mks_constants import G, mSun, AU, day
+
+if 'B-3' in WhichDir:
+	objs = ['AlCenA','AlCenB','PrxCen']+['M'+str(i) for i in range(1,n+1)]
+	if (any([s in WhichDir for s in ['071714','072314','081414']] ) ):
+		m = np.array([1.105,0.934,0.123])*mSun
+	else:
+		m = np.array([0.123,0.123,0.123])*mSun		
+elif 'B-2' in WhichDir:
+	objs = ['AlCenA','AlCenB']+['M'+str(i) for i in range(1,n+1)]
+	if (any([s in WhichDir for s in ['071714','072314','081414']] ) ):
+		m = np.array([1.105,0.934])*mSun
+	else:
+		m = np.array([0.123,0.123])*mSun		
+
+#xv_AU = [[] for obj in objs]
+#xv    = [[] for obj in objs]
+#nt    = [ 0 for obj in objs]
+#aei   = [[] for obj in objs]
+
+### First read in AlCenB to get maximum timesteps
+xvB_AU = AC.ReadAei(WhichDir, objs[1], None,None)
+maxT = xvB_AU.shape[0]
+xvB = AC.AUtoMKS(xvB_AU)
+
+### Make 3D arrays to fill, based on these measurements
+xv  = np.zeros( (len(objs), maxT, 6) )
+xv[1,:,:] = xvB
+
+aei = np.zeros( (len(objs), maxT, 3) )
+a = np.zeros( (len(objs), maxT) )
+e = np.zeros( (len(objs), maxT) )
+i = np.zeros( (len(objs), maxT) )
+
+#j=2
+#obj=objs[j]
+for j in range(2,len(objs)):
+	thisxv_AU = AC.ReadAei(WhichDir, objs[j], None,None)
+	thisT     = thisxv_AU.shape[0]
+	thisxv    = AC.AUtoMKS(thisxv_AU)
+	xv[j,:thisT,:] = thisxv
+
+### Get AB binary
+aB, eB, iB, epsB, xvCM_AB, kB, uB, rB, vB = AC.Binary(
+			[ m[0], m[1] ], 
+			np.array([ xv[0,:,:], xv[1,:,:] ]), 
+			maxT)
+aei[1,:,0],aei[1,:,1],aei[1,:,2] = aB/AU, eB, iB
+a[1,:]    ,    e[1,:],    i[1,:] = aB/AU, eB, iB
+
+### Get small obj orbits relative to B
+for j in range(2,len(objs)):
+	aM, eM, iM, eps, xvCM, k, u, r, v = AC.Binary(
+			[ m[1], 0. ], 
+			np.array([ xv[1,:,:], xv[j,:,:] ]), 
+			maxT)
+	aei[j,:,0],aei[j,:,1],aei[j,:,2] = aM/AU, eM, iM
+	a[j,:]    ,    e[j,:],    i[j,:] = aM/AU, eM, iM
+
+np.savetxt(WhichDir+'/Out/AeiOutFiles/a.out',np.transpose(a),fmt='%.2e',
+	header=''.join([o.rjust(9) for o in objs]))
+np.savetxt(WhichDir+'/Out/AeiOutFiles/e.out',np.transpose(e),fmt='%.2e',
+	header=''.join([o.rjust(9) for o in objs]))
+np.savetxt(WhichDir+'/Out/AeiOutFiles/i.out',np.transpose(i),fmt='%.2e',
+	header=''.join([o.rjust(9) for o in objs]))
+
