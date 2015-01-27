32b8de8bcf043d0385c36da27b1cad6c0df5522b
RJWorth <rjw274@psu.edu>
2015-01-27 10:12:22 -0500

    importing different stuff

--- Sims/ImportExport.bash
+++ Sims/ImportExport.bash
@@ -11,11 +11,11 @@ host=rjw274@hammer.rcc.psu.edu
 h=$(pwd)
 echo $h
 
-### Older true-mass batch of sims
-#pre='Proxlike/071714/Prx'
-#Dir=(1 2 3 4 5 6 7 8 9 10 11 12)
-#suf='-True'
-#tag='0717'
+### Older true-mass batches of sims
+pre='Proxlike/071714/Prx'
+Dir=(1 2 3 4 5 6 7 8 9 10 11 12)
+suf='-True'
+tag='0717'
 
 
 #pre='Proxlike/072314/Prx'
@@ -23,10 +23,10 @@ echo $h
 #suf='-True'
 #tag='0723'
 
-pre='Proxlike/081414/Prx'
-Dir=(01) # 02 03 04 05 06 07 08)
-suf='-True'
-tag='0814'
+#pre='Proxlike/081414/Prx'
+#Dir=(01 02 03 04 05 06 07 08)
+#suf='-True'
+#tag='0814'
 
 ### More recent equal-mass batch
 #pre='Proxlike/Prx'
@@ -35,20 +35,25 @@ tag='0814'
 #suf='-Equal'
 #tag=''
 
-for i in ${Dir[*]}; do
+#for i in ${Dir[*]}; do
 
-	d=$pre$i
-	echo '--------------------------------'$d'--------------------------------'
-	scp -p $host:'~/work/AlphaCen/Sims/'$d'/DiskSurv.eps' Proxlike/Plots/DiskSurv$suf$tag'-'$i.eps
+#	d=$pre$i
+#	echo '--------------------------------'$d'--------------------------------'
+#	scp -p $host:'~/work/AlphaCen/Sims/'$d'/DiskSurv.eps' Proxlike/Plots/DiskSurv$suf$tag'-'$i.eps
 
-done
+#done
 
-#gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dSAFER -sOutputFile=Proxlike/Plots/All-DiskSurv${suf}.pdf Proxlike/Plots/DiskSurv${suf}*.eps
+#gs -dBATCH -dNOPAUSE -dPDFFitPage -dSAFER -q -dDEVICEWIDTHPOINTS=360 -dDEVICEHEIGHTPOINTS=270 -sDEVICE=pdfwrite -sOutputFile=Proxlike/Plots/All-DiskSurv${suf}.pdf Proxlike/Plots/DiskSurv${suf}*.eps
 
-
-
-if [ $suf = '-Equal' ]; then
+### Update all DiskSummary files
+#if [ $suf = '-Equal' ]; then
+	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/DiskSummary.txt' Proxlike/Plots/DiskSummary-Equal.txt
 	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/PrxDisksSurvival.pdf' Proxlike/Plots/PrxDisksSurvival.eps
-	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/DiskSummary.txt' Proxlike/Plots/DiskSummary.txt
-fi
+#elif [ $tag = '0717' ]; then
+	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/071714/DiskSummary.txt' Proxlike/Plots/DiskSummary-0717.txt
+#elif [ $tag = '0723' ]; then
+	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/072314/DiskSummary.txt' Proxlike/Plots/DiskSummary-0723.txt
+#elif [ $tag = '0814' ]; then
+	scp -p $host:'~/work/AlphaCen/Sims/Proxlike/081414/DiskSummary.txt' Proxlike/Plots/DiskSummary-0814.txt
+#fi
 
