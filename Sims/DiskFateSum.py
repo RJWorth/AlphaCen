9e05d2434f1b554fe37774083343fda9a9c04e54
RJWorth <rjw274@psu.edu>
2015-02-13 10:46:41 -0500

    Seems to work now

--- Sims/DiskFateSum.py
+++ Sims/DiskFateSum.py
@@ -12,10 +12,11 @@ def DiskFateSum(WhichDirs='default'):
 		a, dest2, b, c, d = AC.ReadInfo(dr+'/DiskB-2')
 		a, dest3, b, c, d = AC.ReadInfo(dr+'/DiskB-3')
 		dest2,dest3=np.array(dest2),np.array(dest3)
-		fates2 = np.array([len(dest2),
-				sum(dest2=='ejected'),sum(dest2=='AlCenB'),sum(dest2=='Center')])
-		fates3 = np.array([len(dest3),
-				sum(dest3=='ejected'),sum(dest3=='AlCenB'),sum(dest3=='Center')])
-		print(dr+'    {0}     {1}'.format(fates2,fates3))
+		fates2 = np.array([len(dest2),sum(dest2=='ejected'),
+				sum(dest2=='AlCenB'),sum(dest2=='Center'),sum(dest2=='PrxCen')])
+		fates3 = np.array([len(dest3),sum(dest3=='ejected'),
+				sum(dest3=='AlCenB'),sum(dest3=='Center'),sum(dest3=='PrxCen')])
+		print(dr+'    {0} {1}     {2} {3}'.format(sum(fates2[1:]),fates2,
+							  sum(fates3[1:]),fates3 ))
 
 
