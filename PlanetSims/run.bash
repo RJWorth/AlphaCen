34f9a14456b59658e076339d1a6f5268f5f4fb6c
RJWorth <rjw274@psu.edu>
2015-07-21 12:33:13 -0400

    added loc variable for location to put infiles

--- PlanetSims/run.bash
+++ PlanetSims/run.bash
@@ -6,13 +6,14 @@ Dirs=(01)
 
 
 for i in ${Dirs[*]}
-do
+do	
+	loc=$i/In/
 	# write param.in file
-	python -c "import Merc as M; M.WriteParamInFile(loc='"$i"/In/', tf=365.25e0, mStar=1.105, rEj=100, rStar=0.005)"
+	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e0, mStar=1.105, rEj=100, rStar=0.005)"
 
 	# write big.in file with stars+planetesimals
 	# (currently only has planetesimals around AlCenA)
-	python -c "import Merc as M; M.WriteObjInFile(objlist=M.GetObjList(), loc='"$i"/')"
+	python -c "import Merc as M; M.WriteObjInFile(objlist=M.GetObjList(), loc='"$loc"')"
 
 	# run sim in 01 directory
 	cd $i
