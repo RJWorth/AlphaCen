35442ea966c801d2b6ec355cc9ea342043e8b424
RJWorth <bot.collins@gmail.com>
2015-07-22 09:19:21 -0400

    1e1 yr run

--- PlanetSims/run.bash
+++ PlanetSims/run.bash
@@ -9,7 +9,7 @@ for i in ${Dirs[*]}
 do	
 	loc=$i/In/
 	# write param.in file
-	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e0, mStar=1.105, rEj=100, rStar=0.005)"
+	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e1, mStar=1.105, rEj=100, rStar=0.005)"
 
 	# write big.in file with stars+planetesimals
 	# (currently only has planetesimals around AlCenA)
