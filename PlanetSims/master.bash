6637d90de85a1eceeba9cd05a3dd98ae5adff5eb
RJWorth <rjw274@psu.edu>
2015-07-23 09:38:18 -0400

    renamed run files; stop tracking work dirs

---
+++ PlanetSims/master.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+############################################################################### 
+
+machine=$(hostname -s)
+home=$(pwd)
+t=0
+
+if [ $machine = Mirka ]; then
+Dirs=(01 02)
+fi
+if [ $machine = shapiro ]; then
+Dirs=(01 02 03 04 05 06 07)
+fi 
+
+for i in ${Dirs[*]}
+do	
+
+	# run sim in 01 directory
+	./run.bash $i $t > $i/run.pipe &
+	echo 'started '$i': '$!
+
+done	# i in Dirs
+
--- PlanetSims/run.bash
+++ PlanetSims/run.bash
@@ -1,24 +1,40 @@
 #!/bin/bash
-############################################################################### 
+##############################################################################
+# This script cleanly reruns the most recent Merc95
+t1=$(date +%s)
 
-home=$(pwd)
-Dirs=(01 02 03 04 05 06 07)
-t=2
+c=../Code
+in=In/
 
-for i in ${Dirs[*]}
-do	
-	loc=$i/In/
-	# write param.in file
-	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e'"$t"', mStar=1.105, rEj=100, rStar=0.005)"
+cd $1
 
-	# write big.in file with stars+planetesimals
-	# (currently only has planetesimals around AlCenA)
-	python -c "import Merc as M; M.WriteObjInFile(objlist=M.GetObjList(), loc='"$loc"')"
+### Clean previous sim
+rm Out/*
+rm Aei/*
 
-	# run sim in 01 directory
-	cd $i
-	./run.sh > run.pipe &
-	echo 'started '$i': '$!
-	cd ..
-done	# i in Dirs
+# write param.in file
+python -c "import Merc as M; M.WriteParamInFile(loc='"$in"', tf=365.25e"$2", mStar=1.105, rEj=100, rStar=0.005)"
+
+# write big.in file with stars+planetesimals (currently only has planetesimals around AlCenA)
+python -c "import Merc as M; M.WriteObjInFile(objlist=M.GetObjList(), loc='"$in"')"
+
+### Compile and run
+gfortran -w -o merc $c/mercury6_2.f95 $c/drift.f95 $c/orbel.f95 $c/mal.f95 $c/mce.f95 $c/mco.f95 $c/mdt.f95 $c/mio.f95 $c/mfo.f95 $c/mxx.f95 $c/both.f95
+./merc
+gfortran -w -o elem $c/element6.f95 $c/e_sub.f95 $c/orbel.f95 $c/both.f95
+./elem
+
+### Organize files
+mv *.tmp Out
+mv *.aei Aei
+
+### Print runtime
+t2=$(date +%s)
+python -c 'import Merc; print(Merc.WriteRuntime('$t1','$t2'))'
+
+cd ..
+
+dt=$(python -c 'print('$t2'-'$t1')')
+echo $1'    '$t'    '$dt >> runtimes.txt
+
 
