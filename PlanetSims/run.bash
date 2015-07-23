b910c5dcbcf2141d9535442b33f0fa6ad5479346
RJWorth <bot.collins@gmail.com>
2015-07-23 09:17:44 -0400

    More dirs, changed time

--- PlanetSims/run.bash
+++ PlanetSims/run.bash
@@ -2,14 +2,14 @@
 ############################################################################### 
 
 home=$(pwd)
-Dirs=(01 02)
-
+Dirs=(01 02 03 04 05 06 07)
+t=2
 
 for i in ${Dirs[*]}
 do	
 	loc=$i/In/
 	# write param.in file
-	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e0, mStar=1.105, rEj=100, rStar=0.005)"
+	python -c "import Merc as M; M.WriteParamInFile(loc='"$loc"', tf=365.25e'"$t"', mStar=1.105, rEj=100, rStar=0.005)"
 
 	# write big.in file with stars+planetesimals
 	# (currently only has planetesimals around AlCenA)
