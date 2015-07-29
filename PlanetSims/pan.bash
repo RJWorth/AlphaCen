005329a6dd28730cfdc288c9bfb565ba912a596a
RJWorth <bot.collins@gmail.com>
2015-07-29 14:32:55 -0400

    added all Cdir names, and changed printout to improve readability

--- PlanetSims/pan.bash
+++ PlanetSims/pan.bash
@@ -10,16 +10,16 @@ fi
 if [ $machine = shapiro ]; then
 Dirs=(S01 S02 S03 S04 S05 S06 S07)
 fi
-if [ $machine = chloe]; then
-Dirs=(C01)
+if [ $machine = chloe ]; then
+Dirs=(C01 C02 C03 C04 C05 C06 C07 C08 C09 C10)
 fi 
 
 for i in ${Dirs[*]}; do	
-	echo '=============================== '$i' ================================'
+	echo ' * * * * * * * * * * * * * * * * '$i' * * * * * * * * * * * * * * * * *'
 	awk 'NR==26{print;exit}' $i/Out/info.out
-	echo '  * * * * * * * * * * * * * run.pipe * * * * * * * * * * * * * *'
+	echo '  ========================= run.pipe ==========================='
 	tail $n $i/run.pipe
-	echo '  * * * * * * * * * * * * * info.out * * * * * * * * * * * * * *'
+	echo '  ========================= info.out ==========================='
 	tail $n $i/Out/info.out
 done
 
