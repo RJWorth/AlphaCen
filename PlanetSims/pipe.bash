4cdf730af28e6902404feb30a2599c6ead0d83aa
RJWorth <rjw274@psu.edu>
2015-08-03 17:07:22 -0400

    script to streamline scp-ing files back and forth to chloe

---
+++ PlanetSims/pipe.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+##############################################################################
+
+machine=rjw274@chloe.astro.psu.edu
+dir=C05
+
+scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei' $dir
+scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/In/big.in' $dir/In
+scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/info.out' $dir/Out
+scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/element.out' $dir/Out
+
+
