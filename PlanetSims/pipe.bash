1f3d36dbd707ef13d7f4b8b79619dcc2143e0057
RJWorth <rjw274@psu.edu>
2015-08-13 10:50:09 -0400

    only download the used .aei files for the directories with errors (C01 and C06)

--- PlanetSims/pipe.bash
+++ PlanetSims/pipe.bash
@@ -2,11 +2,22 @@
 ##############################################################################
 
 machine=rjw274@chloe.astro.psu.edu
-dir=C01
+dir=$1
 
-scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei' $dir
-scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/In/big.in' $dir/In
-scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/info.out' $dir/Out
-scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/element.out' $dir/Out
+#scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/In/big.in' $dir/In
+#scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/info.out' $dir/Out
+#scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Out/element.out' $dir/Out
 
+### download all, or a subset, of the .aei files
 
+if [ $dir = 'C01' ];then
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/AlCenA.aei' $dir/Aei
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/P0018.aei' $dir/Aei
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/P0053.aei' $dir/Aei
+elif [ $dir = 'C06' ];then
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/AlCenA.aei' $dir/Aei
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/P0003.aei' $dir/Aei
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei/P0911.aei' $dir/Aei
+else
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei' $dir
+fi
