76e81c6a600a9fe76a89d4309e53449e7b333de3
RJWorth <rjw274@psu.edu>
2015-08-13 11:40:52 -0400

    reconfigured run scripts to have variables in a separate include file

--- PlanetSims/master.bash
+++ PlanetSims/master.bash
@@ -2,34 +2,28 @@
 set -e  # stop script on error
 ############################################################################### 
 
-machine=$(hostname -s)
-home=$(pwd)
-
 ### Sim parameters
-ti=0
-tf=3
-rtr=5.0  #3.08
-rice=2.7
-alpha=1.5
-sigma=3.
+incl=master.inc
+source $incl
 
+### Determine relevant directories
 if [ $machine = Mirka ]; then
-Dirs=(L01)
-fi
-if [ $machine = shapiro ]; then
+Dirs=()
+elif [ $machine = shapiro ]; then
 Dirs=(S01 S02 S03 S04 S05 S06 S07)
-fi
-if [ $machine = chloe ];then
-#Dirs=(C01 C02 C03 C04 C05 C06 C07 C08 C09 C10)
-Dirs=(C06)
+Dirs=()
+elif [ $machine = chloe ];then
+#Dirs=(C01 C02 C03 C04 C05 C06 C07 C08 C09 C10 C11 C12)
+Dirs=()
+else
+echo '*** unrecognized machine; not running anything ***'
 fi 
 
-for i in ${Dirs[*]}
-do	
+### Start run for each directory on this machine
+echo $Dirs
 
-	# run sim in 01 directory
-	nice -n 10 ./run.bash $i $ti $tf $rtr $sigma > $i/run.pipe 2>&1 &
+for i in ${Dirs[*]}; do	
+	nice -n 10 ./run.bash $i $incl > $i/run.pipe 2>&1 &
 	echo 'started '$i': '$!
-
 done	# i in Dirs
 
