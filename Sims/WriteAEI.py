e7c30b1fa98ccfce8ace7f20633306e203aab6b3
RJWorth <bot.collins@gmail.com>
2016-01-20 13:56:24 -0500

    Start of script to plot pericenter vs time

--- Sims/WriteAEI.py
+++ Sims/WriteAEI.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 import AlphaCenModule as AC
+import matplotlib.pyplot as plt
+
 
 Dirs1 = [       'Proxlike/Prx'+'{0:02}'.format(i)+'/Original' for i in range(1,35)]
 Dirs2 = ['Proxlike/071714/Prx'+  '{0:}'.format(i)+'/Original' for i in range(1,13)]
@@ -9,10 +11,9 @@ Dirs4 = ['Proxlike/081414/Prx'+'{0:02}'.format(i)+'/Original' for i in range(1,9
 
 Dirs = Dirs1+Dirs2+Dirs3+Dirs4
 ndir = len(Dirs)
+minpC = np.array(len(Dirs))
 
-print(Dirs)
-print(Dirs[0])
-for d in [Dirs[0]]:
+for i,d in enumerate([Dirs[0]]):
 ### Create TimeData.txt file for each, in Out/Aei...
 ##	AC.WriteAEI(d)
 
@@ -20,11 +21,33 @@ for d in [Dirs[0]]:
 	timedata = np.loadtxt(d+'/Out/AeiOutFiles/TimeData.txt', skiprows=1)
 	header = open(d+'/Out/AeiOutFiles/TimeData.txt' ,'r').readline().split()
 
-	print(timedata)
 	print(header)
-	print(timedata.shape)
+	assert len(header) == timedata.shape[1],'Header length and # columns do not match!'
+
+	dict = {}
+	for j,h in enumerate(header):
+		dict[h] = timedata[:,j]
 
 ### Get C's closest approach, save to array
-	
+	minpC[i] = np.min(dict['pC'])	
+	print(dict.items())
+
+	minloc = np.where(dict['pC'] == minpC[i])
+	print(dict['pC'][minloc])
+	print(dict['t'][minloc])
+	print(dict['pB'][minloc])
+
+	##### Make plot
+	f, ax1 = plt.subplots(1)
+	ax.scatter(dict['t'],dict['pC'])
+	ax1.set_xscale('log')
+	ax1.set_yscale('log')
+	ax1.set_xlabel('Time')
+	ax1.set_ylabel('Pericenter')
+	  	
+	plt.savefig(d+'/PvsT.pdf')
+	plt.clf()
+	plt.close(f)
+
 
 
