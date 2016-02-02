8cff5750fdd16b8e21eab3afded92fc5abcd7c3e
RJWorth <rjw274@psu.edu>
2016-02-02 17:07:08 -0500

    uniform spacing, no tabs

--- mks_constants.py
+++ mks_constants.py
@@ -2,22 +2,22 @@
 
 
 ### Define values for constants
-AU			= 1.49597871e11	# m
-day			= 24*3600.		# s
-G			= 6.67384e-11	# m^3/kg/s^2 
-mSun		= 1.9891e30		# kg
+AU          = 1.49597871e11	# m
+day         = 24*3600.		# s
+G           = 6.67384e-11	# m^3/kg/s^2 
+mSun        = 1.9891e30		# kg
 mEarth      = 5.972e24 		# kg
 mMoon       = 7.34767309e22 # kg
 mMars       = 6.41693e23    # kg
 
 ### Masses for AlphaCen system
-#mA, mB, mC = 1.105*mSun, 0.934*mSun, 0.123*mSun
-#m 			= [mA, mB, mC]
-#M 			= mA+mB+mC
-#mu 			= G*M
+#mA, mB, mC  = 1.105*mSun, 0.934*mSun, 0.123*mSun
+#m           = [mA, mB, mC]
+#M           = mA+mB+mC
+#mu          = G*M
 
 ### Conversion factors
 from math import pi
-deg2rad = 2*pi/360.
-rad2deg = 360./2*pi
+deg2rad     = 2*pi/360.
+rad2deg     = 360./2*pi
 
