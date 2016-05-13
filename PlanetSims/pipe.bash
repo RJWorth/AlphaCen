47fb13a3ad4e2df9a2daf30a17a0df1489e2f9e8
RJWorth <rjw274@psu.edu>
2016-05-13 17:18:58 -0400

    uncomment scp lines, and correct Aei file detection

--- PlanetSims/pipe.bash
+++ PlanetSims/pipe.bash
@@ -13,14 +13,15 @@ dir=Data/$1
 files=(In/big.in In/small.in In/param.in Out/info.out Out/element.out Out/param.dmp)
 for f in ${files[*]}; do
 	if [ ! -f $dir/$f ];then
-#	scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/'$f $dir/$f
-	echo $dir'/'$f 'missing'
+	scp -p  $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/'$f $dir/$f
+#	echo $dir'/'$f 'missing'
 	fi
 done
 
 ### download the .aei directory, if not already present
-if [ ! -f $dir/Aei/AlCenB.aei ];then
-#	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei' $dir
-	echo $dir/aei missing
+if [ ! -f $dir/Aei/AlCenA.aei ];then
+	scp -rp $machine:'/Volumes/Macintosh\ HD\ 2/rjw/ACPltSims/'$dir'/Aei' $dir
+#	echo $dir/aei missing
 fi
 
+#sleep 30
