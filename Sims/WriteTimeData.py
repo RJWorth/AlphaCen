c5e92c81ea4505690ab04cfe0b0ef805de65c51b
RJWorth <rjw274@psu.edu>
2016-05-14 12:10:35 -0400

    adjustments to rewriting TimeData script

--- Sims/WriteTimeData.py
+++ Sims/WriteTimeData.py
@@ -15,11 +15,12 @@ Dirs2 = ['Proxlike/071714/Prx'+  '{0:}'.format(i) for i in range(1,13)]
 Dirs3 = ['Proxlike/072314/Prx'+'{0:02}'.format(i) for i in range(1,27)]
 Dirs4 = ['Proxlike/081414/Prx'+'{0:02}'.format(i) for i in range(1,9)]
 
-#Dirs = Dirs1+Dirs2+Dirs3+Dirs4
-Dirs = Dirs1+Dirs4
+Dirs = Dirs1+Dirs2+Dirs3+Dirs4
+#Dirs = Dirs1+Dirs4
 
 #subdirs = ['Original','DiskA-2','DiskA-3','DiskB-2','DiskB-3']
-subdirs = ['DiskA-2','DiskA-3']
+subdirs = ['Original', 'DiskB-2', 'DiskB-3']
+#subdirs = ['DiskA-2','DiskA-3']
 
 ### Write TimeData.txt file for each Dir and subdir
 for i,d in enumerate(Dirs):
@@ -27,6 +28,6 @@ for i,d in enumerate(Dirs):
 		thisdir = d+'/'+s
 		print(thisdir)
 		AC.WriteTimeData(thisdir)
-
+		#AC.PlotDisk(AC.CalcDisk(thisdir))
 
 
